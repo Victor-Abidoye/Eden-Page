4681d2c51da0153a38591d2001a8e46d5de881f1
Victor Abidoye <abidoye8224@gmail.com>
2023-02-19 11:14:55 +0100

    Design mobile and desktop view of page 11

--- main.js
+++ main.js
@@ -7,10 +7,22 @@ jsConfetti.addConfetti({
 const eight = document.querySelector('#main_eight')
 const nine = document.querySelector('#main_nine')
 const homeBtn = document.querySelector('#progress')
+const foodModal = document.querySelector('#food')
+const cleanModal = document.querySelector('#clean')
 
 const progress = () => {
     eight.classList.add('hide')
     nine.classList.remove('hide')
 }
+
+
+const toggleModal = (item) => {
+    if (item === 'food') {
+        foodModal.classList.toggle('hide')
+    } else if (item === 'clean') {
+        cleanModal.classList.toggle('hide')
+    }
+}
+
 
 homeBtn.addEventListener('click', progress)
